464b11494bb5012dfbf8f76796c40e5051f34a89
Романов Александр Сергеевич <aromanov2288@gmail.com>
2019-02-19 22:45:22 +0300

    Initial commit

---
+++ src/main/java/ru/romanov/modulefour/Application.java
@@ -0,0 +1,13 @@
+package ru.romanov.modulefour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
